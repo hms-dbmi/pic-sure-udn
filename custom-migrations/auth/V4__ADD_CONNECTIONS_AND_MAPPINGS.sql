24c4153899eab02b8c114281fbdf07348bb2f887
Jason Stedman <namdets@gmail.com>
2020-12-09 12:03:23 -0500

    adding connections and userMetadataMappings for new SAML connections

---
+++ custom-migrations/auth/V4__ADD_CONNECTIONS_AND_MAPPINGS.sql
@@ -0,0 +1,19 @@
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0x2872759ea98f402599f599159e8a95ab,'Baylor Genetics','baylorgenetics','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0xc11baf2e2d3f4307a683eef0369f41db,'CHOP','chop','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0x16e7928916064f6ba98c5e9758b0f1cd,'Miami','miami','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0xf8b80f986caf438581551e558f38fbfd,'Partners','partners','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0x964b6c7585354287b122e81649d592b7,'Seattle Childrens','seattle-childrens','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0xf3c34adb891e4c42a2323e4618baf647,'UPenn','upenn','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0xefc741f9f9d943099589d27195391055,'UUtah','utah','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+INSERT INTO connection (uuid, label, id, subprefix, requiredFields) VALUES (0x1139456862364c40a86b68a750830885,'Vanderbilt UMC','vumc','samlp|','[{\"label\":\"Email\", \"id\":\"email\"}]');
+
+INSERT INTO userMetadataMapping VALUES 
+(0x2872759ea98f402599f599159e8a95ab,'$.email',0x2872759ea98f402599f599159e8a95ab,'$.email') ,
+(0xc11baf2e2d3f4307a683eef0369f41db,'$.email',0xc11baf2e2d3f4307a683eef0369f41db,'$.email') ,
+(0x16e7928916064f6ba98c5e9758b0f1cd,'$.email',0x16e7928916064f6ba98c5e9758b0f1cd,'$.email') ,
+(0xf8b80f986caf438581551e558f38fbfd,'$.email',0xf8b80f986caf438581551e558f38fbfd,'$.email') ,
+(0x964b6c7585354287b122e81649d592b7,'$.email',0x964b6c7585354287b122e81649d592b7,'$.email') ,
+(0xf3c34adb891e4c42a2323e4618baf647,'$.email',0xf3c34adb891e4c42a2323e4618baf647,'$.email') ,
+(0xefc741f9f9d943099589d27195391055,'$.email',0xefc741f9f9d943099589d27195391055,'$.email') ,
+(0x1139456862364c40a86b68a750830885,'$.email',0x1139456862364c40a86b68a750830885,'$.email');
+
